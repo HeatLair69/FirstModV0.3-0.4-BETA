4a4179a2c88f8013db3cf648ce62df1dade45658
HeatLair69 <maxthebomb99@gmail.com>
2024-06-23 20:29:34 +0300

    beta 0.2

--- src/main/java/net/firstmod/FirstMod.java
+++ src/main/java/net/firstmod/FirstMod.java
@@ -2,6 +2,7 @@ package net.firstmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.firstmod.block.ModBlocks;
 import net.firstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,5 +14,6 @@ public class FirstMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerBlocks();
 	}
 }
--- src/main/java/net/firstmod/item/ModItems.java
+++ src/main/java/net/firstmod/item/ModItems.java
@@ -4,6 +4,8 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.firstmod.FirstMod;
+import net.firstmod.block.ModBlocks;
+import net.minecraft.block.TntBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -14,10 +16,16 @@ public class ModItems {
 
     public static final Item PENIS = registerItem("penis", new Item(new FabricItemSettings()));
     public static final Item URANIUM = registerItem("uranium", new Item(new FabricItemSettings()));
+    public static final Item RAW_URANIUM = registerItem("raw_uranium", new Item(new FabricItemSettings()));
 
     private static void addItemsToIngridientTabItemGroup(FabricItemGroupEntries entries) {
         entries.add(PENIS);
         entries.add(URANIUM);
+        entries.add(RAW_URANIUM);
+        entries.add(ModBlocks.URANIUM_BLOCK);
+        entries.add(ModBlocks.URANIUM_ORE);
+        entries.add(ModBlocks.NUKE);
+        entries.add(ModBlocks.DEEPSLATE_URANIUM_ORE);
     }
 
     private static Item registerItem(String name, Item item) {
